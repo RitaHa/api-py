607549a0730c625cc9ed6d51896f90ba74dc2683
Rita Hancharenka <ritahancharenka@Ritas-MacBook-Pro.local>
2024-01-21 18:14:56 +0100

    Add sample api checks

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BASE_URI = 'https://jsonplaceholder.typicode.com'
-POSTS = f'{BASE_URI}/posts'
-POST_DETAILS = f'{BASE_URI}/posts/1'
+BASE_URL = 'https://jsonplaceholder.typicode.com'
+POSTS_URI = f'{BASE_URL}/posts'
+POST_DETAILS_URI = f'{BASE_URL}/posts/1'
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,25 @@
+from json import dumps
+
 import requests
 
-from config import POSTS, POST_DETAILS
+from config import POSTS_URI, POST_DETAILS_URI
 
 
 def test_get_all_posts():
-    response = requests.get(POSTS)
+    user_id = 1
+    response = requests.get(POSTS_URI)
+    response_json = response.json()
     assert response.status_code == requests.codes.ok
+    user_ids = [post['userId'] for post in response_json]
+    assert user_id in user_ids
 
 
 def test_get_post_details():
-    expected_data = {"userId": 1,
-                     "id": 1,
-                     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}
-    response = requests.get(POST_DETAILS)
+    expected_data = {'userId': 1,
+                     'id': 1,
+                     'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+                     'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+    response = requests.get(POST_DETAILS_URI)
     response_json = response.json()
     assert response.status_code == requests.codes.ok
     for key, value in expected_data.items():
@@ -21,25 +27,30 @@ def test_get_post_details():
 
 
 def test_post_new_post():
-    response = requests.post(POST_DETAILS)
-    assert response.status_code == requests.codes.ok
+    data = {'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+            'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json'}
+    payload = dumps(data)
+    response = requests.post(POSTS_URI, data=payload, headers=headers)
+    assert response.status_code == requests.codes.created
     post_id = response.json()['id']
     assert isinstance(post_id, int)
 
 
 def test_put_post():
-    response = requests.put(POST_DETAILS)
+    response = requests.put(POST_DETAILS_URI)
     assert response.status_code == requests.codes.ok
     post_id = response.json()['id']
     assert isinstance(post_id, int)
 
 
 def test_patch_post():
-    expected_data = {"userId": 1,
-                     "id": 1,
-                     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}
-    response = requests.patch(POST_DETAILS)
+    expected_data = {'userId': 1,
+                     'id': 1,
+                     'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+                     'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+    response = requests.patch(POST_DETAILS_URI)
     response_json = response.json()
     assert response.status_code == requests.codes.ok
     for key, value in expected_data.items():
@@ -47,7 +58,7 @@ def test_patch_post():
 
 
 def test_delete_post():
-    response = requests.delete(POST_DETAILS)
+    response = requests.delete(POST_DETAILS_URI)
     response_json = response.json()
     assert response.status_code == requests.codes.ok
     assert len(response_json) == 0
